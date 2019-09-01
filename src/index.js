a0443a34e1b3caa7c2f2cfe55947b244a9b59e19
Wioletta Gluza <wiolagluza@gmail.com>
2019-09-01 23:39:01 +0200

    react suspense fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -6,7 +6,12 @@ import * as serviceWorker from './serviceWorker';
 // import i18n (needs to be bundled ;))
 import './i18n';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Suspense fallback="loading...">
+    <App />
+  </Suspense>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
